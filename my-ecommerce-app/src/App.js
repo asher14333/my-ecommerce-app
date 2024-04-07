dae63e4d18543c97f9a9f3d592a10cc51f009b80
YaseenMR <Yaaseenmrashid@gmail.com>
2024-04-07 01:10:40 -0600

    .

--- my-ecommerce-app/src/App.js
+++ my-ecommerce-app/src/App.js
@@ -4,6 +4,7 @@ import { AuthProvider } from './components/Authenticator';
 import Homepage from './components/Homepage';
 import Productpage from './components/Productpage';
 import LoginPage from './components/LoginPage';
+import Authenticator from './components/AuthenticationChecker';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route index element={<Homepage />} />
           <Route path="/Productpage" element={<Productpage />} />
+          {/* <Route path="/Productpage" element={<Authenticator> <Productpage /> </Authenticator>} />      */}
           <Route path="/login" element={<LoginPage />} />
         </Routes>
       </Router>
